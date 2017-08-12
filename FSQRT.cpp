32e7f68fe223ca8f69dcc67c688ac154f3723edc
Ashwin Patil <theashwinpatil@gmail.com>
2017-08-13 00:33:45 +0530

    Create FSQRT.cpp

---
+++ FSQRT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	long long int t;
+	cin>>t;
+	while (t--) {
+		int a;
+		cin>>a;
+		cout<<(int) sqrt(a)<<endl;
+	}
+	return 0;
+}
