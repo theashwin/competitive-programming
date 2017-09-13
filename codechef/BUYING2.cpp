b2f6fd4f71b90bf4bd53f01f21fc0be8cb91b097
Ashwin Patil <theashwinpatil@gmail.com>
2017-09-13 21:06:39 +0530

    Create BUYING2.cpp

---
+++ codechef/BUYING2.cpp
@@ -0,0 +1,68 @@
+//INCLUDE
+#include <bits/stdc++.h>
+using namespace std;
+//DEFINE
+#define gc getchar_unlocked			//char c = gc();
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll	long long
+#define lli	long long int
+#define pb	push_back
+#define pf push_front
+#define mp	make_pair
+#define F 	first
+#define S 	second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626	//to pi or not to pi.
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+//TYPEDEF
+typedef pair<int, int>	pi;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<lli>		vlli;
+typedef vector<ll>		vl;
+typedef vector<pi>		vpi;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+//FUNCTIONS
+ll mpow(int base, int exp) {
+	base %= MOD;
+	int result = 1;
+	while (exp > 0) {
+		if (exp & 1) result = ((ll)result * base) % MOD;
+		base = ((ll)base * base) % MOD;
+		exp >>= 1;
+	}
+	return result;
+}
+
+int main() {
+	lli t;
+	scanf("%lld",&t);
+	while(t--) {
+		lli n,q;
+		cin>>n>>q;
+		vector<lli> v(n);
+		cin>>v[0];
+		lli sum = v[0];
+		lli min = v[0];
+		for(lli i = 1; i < n; i++) {
+			cin>>v[i];
+			sum += v[i];
+			if(min > v[i]) {
+				min = v[i];
+			}
+		}
+		if((sum/q) == (floor((sum-min)/q))) {
+			cout<<"-1"<<endl;
+		} else {
+			cout<<sum/q<<endl;
+		}
+	}
+	return 0;
+}
