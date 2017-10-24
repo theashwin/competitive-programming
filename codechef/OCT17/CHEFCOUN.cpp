6790fdc79919b0424f5283d8b9a9480c55e5b66c
Ashwin Patil <theashwinpatil@gmail.com>
2017-10-24 22:36:48 +0530

    Create CHEFCOUN.cpp

---
+++ codechef/OCT17/CHEFCOUN.cpp
@@ -0,0 +1,68 @@
+//INCLUDE
+#include <bits/stdc++.h>
+using namespace std;
+//DEFINE
+#define gc getchar_unlocked			//char c = gc();
+#define fo(i,n) for (i=0;i<n;i++)
+#define Fo(i,k,n) for (i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll	long long
+#define lli	long long int
+#define pb	push_back
+#define mp	make_pair
+#define F 	first
+#define S 	second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626	//to pi or not to pi.
+#define MOD 1000000007
+#define INF 0x3f3f3f3f
+//TYPEDEF
+typedef pair<int, int>	pi;
+typedef pair<lli, lli>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pi>		vpi;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	lli t;
+	cin>>t;
+	lli mod = pow(2,32);
+	while(t--) {
+		lli n;
+		cin>>n;
+		lli k = floor((mod - 5) / (n-2));
+		lli z = (mod-5) - ((n-2)*k);
+
+		cout<<"2 1 ";
+		for(lli i = 2; i < n; i++) {
+			if(z > 0) {
+				cout<<k+1<<" ";
+				z--;
+			}
+			else {
+				cout<<k<<" ";
+			}
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
+ll mpow(int base, int exp) {
+	base %= MOD;
+	int result = 1;
+	while (exp > 0) {
+		if (exp & 1) result = ((ll)result * base) % MOD;
+		base = ((ll)base * base) % MOD;
+		exp >>= 1;
+	}
+	return result;
+}
