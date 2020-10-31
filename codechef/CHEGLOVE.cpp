0a52377de86be19755bf673de73e6f1bb440e06b
devikasugathan <devikasugathan007@gmail.com>
2020-10-31 08:30:22 +0530

    Added Soln

---
+++ codechef/CHEGLOVE.cpp
@@ -0,0 +1,45 @@
+# include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	for(int i=0;i<t;i++)
+	{ int n,f=0,b=0;
+	 cin>>n;
+	 int l[n],g[n];
+	 for(int j=0;j<n;j++)
+	 {
+	     cin>>l[j];
+
+	 }
+	 for(int j=0;j<n;j++)
+	 {
+	     cin>>g[j];
+
+	 }
+	 for(int j=0;j<n;j++)
+	 {
+	    if(g[j]>=l[j])
+	    f=f+1;
+	    else f=0;
+	 }
+	  for(int j=0,k=n-1;j<n,k>=0;j++,k--)
+	   {
+	       if(g[k]>=l[j])
+	        b=b+1;
+	        else
+	        b=0;
+
+	 }
+	 if(f==n&&b!=n)
+	 cout<<"front"<<endl;
+	 else if(f==n&&b==n)
+	 cout<<"both"<<endl;
+	 else if(b==n&&f!=n)
+	 cout<<"back"<<endl;
+	 else if(b!=n&&f!=n)
+	 cout<<"none"<<endl;
+	}
+return 0;
+}
